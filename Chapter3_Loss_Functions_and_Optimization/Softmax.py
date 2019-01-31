8e1a6fb0fe1df2938e94298c295aefe5167e0116
최연식 <gafield8785@naver.com>
2019-01-31 14:20:01 +0900

    typo

--- Chapter3_Loss_Functions_and_Optimization/Softmax.py
+++ Chapter3_Loss_Functions_and_Optimization/Softmax.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def softmax(a): # 지수함수를 계산할 때 값이 커지면 nan이 계산됨
     c = np.max(a)
-    exp_a = np.exp(a-c) # 소프트 맫그의 지수함수를 계산할 떄 어떤 정수를 더해도 결과는 바뀌지 않는다는 점을 이용
+    exp_a = np.exp(a-c) # 소프트 맥스의 지수함수를 계산할 떄 어떤 정수를 더해도 결과는 바뀌지 않는다는 점을 이용
     sum_exp_a = np.sum(exp_a)
     y = exp_a / sum_exp_a
 
