e71de3861a2ed5c4ea65161fc0a84b9e250b670f
최연식 <gafield8785@naver.com>
2019-01-31 14:18:50 +0900

    typo

--- Chapter3_Loss_Functions_and_Optimization/Cross_Entropy_Error.py
+++ Chapter3_Loss_Functions_and_Optimization/Cross_Entropy_Error.py
@@ -8,8 +8,8 @@ def cross_entropy_error(y, t): # t는 라벨
 t = [0,0,1,0,0,0,0,0,0,0]
 y = [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]
 
-print(cross_entropy_error(np.array(y), np.array(t)))
+print(cross_entropy_error(np.array(y), np.array(t))) # 0.5108254
 
 y = [0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0]
 
-print(cross_entropy_error(np.array(y), np.array(t))) # 틀린 경우 늘어캄
+print(cross_entropy_error(np.array(y), np.array(t))) # 틀린 경우 늘어남 2.302584
