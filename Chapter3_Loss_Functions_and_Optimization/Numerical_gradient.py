4ee578fcc5a71b57bd6cf8bcd5be81edc41cba44
최연식 <gafield8785@naver.com>
2019-01-31 16:46:49 +0900

    add_description

--- Chapter3_Loss_Functions_and_Optimization/Numerical_gradient.py
+++ Chapter3_Loss_Functions_and_Optimization/Numerical_gradient.py
@@ -1,12 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-def numerical_diff(f,x):
-    h = 1e-4 # 너무 작은 숫자 넣으면 계산 X
+def numeircal_diff2(f,x):
+    h = 1e-4
     return (f(x+h) - f(x-h)) / (2*h)
 
-# 예시함수
 def function_1(x):
     return 0.01*x**2 + 0.1*x
 
@@ -14,11 +12,9 @@ x = np.arange(0,20,0.1)
 y = function_1(x)
 plt.xlabel("x")
 plt.ylabel("f(x)")
-plt.title("f(x) = 0.01*x^2 + 0.1*x")
+plt.title("f(x) = 0.01x^2 + 0.1x")
 plt.plot(x,y)
 plt.show()
 
-print(numerical_diff(function_1,5)) # 0.19999999
-print(numerical_diff(function_1,10)) # 0.29999999
-
-# numerical_gradient의 경우 연산속도가 매우 느리다. 뒤에서 배울 back_propagation과 비교할 예정
+print(numerical_diff(function_1,5)) # 0.199999
+print(numerical_diff(function_1,10)) # 0.299999
