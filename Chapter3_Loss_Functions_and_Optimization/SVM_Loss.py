036f15f76ad864bdeee4d84c16acc8269cc3682f
최연식 <gafield8785@naver.com>
2019-02-08 22:46:55 +0900

    markdown_update

--- Chapter3_Loss_Functions_and_Optimization/SVM_Loss.py
+++ Chapter3_Loss_Functions_and_Optimization/SVM_Loss.py
@@ -1,19 +1,24 @@
-def SVM_Loss(y, t): # t는  라벨
-    a = 0
-    for i in range(len(y)):
-        b = max(0, y[i]-y[t]+1)
-        a += b
-    return a - 1
-
-y1 = [3.2, 5.1, -1.7]
-y2 =  [1.3, 4.9, 2.0]
-y3 = [2.2, 2.5, -3.1]
-
-t0 = 0
-t1 = 1
-t2 = 2
+def SVM_Loss(scores, y, safety_margin=1):
+    """
+    scores : w.dot(x)
+    y : 라벨
+    safety_margin : safety_margin
+    """
+    margins = np.maximum(0,scores - scores[y] + safety_margin)
+    margins[y] = 0
+    loss_i = np.sum(margins)
+    return loss_i
 
 
-print(SVM_Loss(y1,t0)) # 2.89
-print(SVM_Loss(y2,t1)) # 0
-print(SVM_Loss(y3,t2)) # 12.9
+scores1 = np.array([3.2, 5.1, -1.7])
+scores2 =  np.array([1.3, 4.9, 2.0])
+scores3 = np.array([2.2, 2.5, -3.1])
+
+y0 = 0
+y1 = 1
+y2 = 2
+
+
+print(SVM_Loss(scores1,y0)) # 2.89
+print(SVM_Loss(scores2,y1)) # 0
+print(SVM_Loss(scores3,y2)) # 12.9
